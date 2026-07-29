9a41282d0e0c9d3e2bc61dbd10a83f3f6535740d
Aaditya Shrestha <aadityastha123456@gmail.com>
2026-07-29 15:18:15 +0545

    add: imported testimonials in app.jsx, and fonts in css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import CaseStudies from "./pages/CaseStudies";
+import Testimonials from "./pages/Testimonials";
 
 function App() {
   return (
@@ -29,6 +30,19 @@ function App() {
         <Home />
       </div>
       <CaseStudies />
+      <div
+        style={{
+          backgroundColor: "#080808",
+          width: "101%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingBottom: "60px",
+        }}
+      >
+        <Testimonials />
+      </div>
     </div>
   );
 }
