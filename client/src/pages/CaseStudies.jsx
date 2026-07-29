e05ef39104949ed91739896359f13943de301731
Aaditya Shrestha <aadityastha123456@gmail.com>
2026-07-29 12:45:31 +0545

    modify: components removed and worked with remaining

---
+++ client/src/pages/CaseStudies.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CaseStudies = () => {
+  return (
+    <div>
+      <div>
+        <p>Case Studies</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+          accusantium veritatis cum nesciunt nam magnam!
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default CaseStudies;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,33 +1,67 @@
 import React from "react";
 import Card from "../components/ui/card";
 import leonardo from "../assets/leonardo.png";
+import Button from "../components/ui/button";
+import profile from "../assets/leonardo.png";
 
 const Home = () => {
   return (
     <div
       style={{
-        diplay: "flex",
+        display: "flex",
         justifyContent: "space-around",
+        alignItems: "center",
         width: "65%",
         marginTop: "6%",
+        gap: "10%",
       }}
     >
-      <Card
-        topic={"Aaditya Shrestha"}
-        desc={
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus atque quibusdam accusamus culpa earum, voluptas nulla, labore, natus in fugit eum aut quaerat inventore debitis! "
-        }
-        label={"Let's get started >"}
-        color={"#ffffff"}
-        bg_color={"#3f8e00"}
-        topic_color={"#ffffff"}
-        topic_size={"35px"}
-        desc_color={"#9c9c9c"}
-        desc_size={"17px"}
-        src={leonardo}
-        borderRadius={"50%"}
-        height={"350px"}
-      />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          gap: "25px",
+        }}
+      >
+        <p
+          style={{
+            color: "#ffffff",
+            fontSize: "44px",
+            marginBottom: 0,
+          }}
+        >
+          Aaditya Shrestha
+        </p>
+        <p
+          style={{
+            color: "#9c9c9c",
+            fontSize: "14px",
+            width: "492px",
+            lineHeight: "20px",
+            wordSpacing: "5px",
+          }}
+        >
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos,
+          temporibus libero modi magnam itaque delectus blanditiis voluptatem ea
+          id quisquam corrupti, facere tempore fuga doloribus obcaecati nemo
+          iste dolore cum.
+        </p>
+        <Button
+          label={"Let's get started >"}
+          color={"white"}
+          padding={"21px 64px"}
+          button_color={"#3f8e00"}
+          fontSize={"16px"}
+        />
+      </div>
+      <div>
+        <img
+          src={profile}
+          alt="my_profile"
+          style={{ border: "none", borderRadius: "50%", height: "416px" }}
+        />
+      </div>
     </div>
   );
 };
