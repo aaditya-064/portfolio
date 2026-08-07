7e54cbe2f12fa0d81d3da325d553ede3bfbf24a8
Aaditya Shrestha <aadityastha123456@gmail.com>
2026-08-07 20:30:28 +0545

    modify: changed the link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavLinks from "./ui/navlinks";
 import { FaLinkedinIn, FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -27,23 +28,41 @@ const Navbar = () => {
           paddingBottom: "24px",
         }}
       >
-        <NavLinks label={"Home"} link={"/"} />
-        <NavLinks label={"Case Studies"} link={"/case-studies"} />
-        <NavLinks label={"Testimonials"} link={"/testimonials"} />
-        <NavLinks label={"Recent work"} link={"/recent-work"} />
-        <NavLinks label={"Get In Touch"} link={"/get-in-touch"} />
+        <NavLinks label={"Home"} link={"#home"} />
+        <NavLinks label={"Case Studies"} link={"#case_studies"} />
+        <NavLinks label={"Testimonials"} link={"#testimonials"} />
+        <NavLinks label={"Recent work"} link={"#recent_work"} />
+        <NavLinks label={"Get In Touch"} link={"#contact"} />
       </div>
       <div
         style={{
           display: "flex",
           gap: "25px",
-          color: "#9c9c9c",
+
           fontSize: "20px",
         }}
       >
-        <FaLinkedinIn />
-        <FaInstagram />
-        <FaXTwitter />
+        <Link
+          to="https://x.com/aadi_shrestha_"
+          style={{ color: "#9c9c9c" }}
+          target="_blank"
+        >
+          <FaLinkedinIn />
+        </Link>
+        <Link
+          to="https://www.instagram.com/aaditya_shrestha__/"
+          style={{ color: "#9c9c9c" }}
+          target="_blank"
+        >
+          <FaInstagram />
+        </Link>
+        <Link
+          to="https://www.linkedin.com/in/aaditya-shrestha-b4032138a"
+          style={{ color: "#9c9c9c" }}
+          target="_blank"
+        >
+          <FaXTwitter />
+        </Link>
       </div>
     </div>
   );
